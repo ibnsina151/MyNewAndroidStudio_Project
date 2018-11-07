0134a73e9e088a4564014e190c823bbf03e04731
ibnsinamunim <ibnsinam12@gmail.com>
2018-11-07 22:15:58 +0600

    Add new Function

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello{
 	public static void public static void main(String[] args) {
 		System.out.println("Vs Code is Awesome..");
+		display("Commit");
+	}
+	public static void display(String name){
+		System.out.println(name);
 	}
 }
